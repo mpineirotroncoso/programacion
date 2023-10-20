7320ce3d53190977b1b51ecf08c5453da8583b70
Marcos <mpineirotroncoso@danielcastelao.org>
2023-10-20 11:31:53 +0200

    :sparkles: se usa else if

--- Boletin07/Boletin07_05/src/boletin07_05/Clase.java
+++ Boletin07/Boletin07_05/src/boletin07_05/Clase.java
@@ -12,11 +12,9 @@ public class Clase {
     public void comprobarMaior(short num1, short num2, short num3){
         if (num1 > num2 && num1 > num3) {
             System.out.println(num1+" Es el mayor");
-        }
-        if (num2 > num1 && num2 > num3) {
+        } else if (num2 > num1 && num2 > num3) {
             System.out.println(num2+" Es el mayor");
-        }
-        if (num3 > num1 && num3 > num2) {
+        } else if (num3 > num1 && num3 > num2) {
             System.out.println(num3+" Es el mayor");
         }
     }
